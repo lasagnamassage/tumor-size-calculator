8d8fa96b1672dd46b84d62725b80faef4bcf3f87
Ashaun Thomas <ashaunthomas@siu.edu>
2022-05-23 13:05:12 -0700

    adds basic server

---
+++ main.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+const port = 8080
+
+let fileName = "index.html";
+
+app.use(express.static('public'))
+
+
+app.get('/', function (req, res, next) {
+    var options = {
+      root: path.join(__dirname, 'public'),
+      dotfiles: 'deny',
+    }
+    res.sendFile(fileName, options, function (err) {
+      if (err) {
+        next(err)
+      } else {
+        console.log('Sent:', fileName)
+      }
+    })
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- public/script.js
+++ public/script.js
@@ -1,12 +1,13 @@
-const input = document.querySelector("input[type =”file”]")
+//uploads image. Stretching will not change ratio.
+const input = document.querySelector('input[type="file"]')
 const img = new Image();
-input.addEventListener("change", function () {
+input.addEventListener('change', function () {
     const reader = new FileReader()
     reader.onload = function () {
 
         img.src = reader.result;
-        var c = document.getElementById('myCanvas');
-        var ctx = c.getContext("2d");
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext('2d');
         ctx.drawImage(img, 0, 0, 1200, 800);
     }
     reader.readAsDataURL(input.files[0])
@@ -17,43 +18,50 @@ var cnt = 0;
 function CountFun() {
     cnt++
     if (cnt >= 1) {
-        window.alert('Tumor / Tissue Percentage: ' + (100 * totaltumor) / (totaltissue) + '%');
+        window.alert("Tumor/Tissue Percentage: " + (100 * totaltumor) / (totaltissue) + "%");
     }
 }
-var cnt1 = 0; 
-var boolean;
 
+var cnt1 = 0; var boolean;
 function CountFun1() {
     cnt1++
-    var divData = document.getElementById('showCount1');
-    divData.innerHTML ='Tumor Pieces: (“+cnt1 +”)”;//this part has been edited'
+    var divData = document.getElementById("showCount1");
+    divData.innerHTML = "Tumor Pieces: (" + cnt1 + ")";//this part has been edited
     boolean = true;
 }
+
 var cnt2 = 0;
 function CountFun2() {
     cnt2++
     var divData = document.getElementById("showCount2");
-    divData.innerHTML ='Tissue Pieces: (“+cnt2 +”)”;//this part has been edited';
+    divData.innerHTML = "Tissue Pieces: (" + cnt2 + ")";//this part has been edited
     boolean = false;
 }
+
+
 var totaltumor = 0;
 var totaltissue = 0;
 
-//Button Funtions
+//Button Funtions       
+
 var pointsarray = [];
+
+
 function addpoint(event) {
     var rect = myCanvas.getBoundingClientRect();
     var docx = (event.clientX - rect.left);
     var docy = (event.clientY - rect.top);
-    var canvas = document.getElementById('myCanvas');
-    var context = canvas.getContext("2d");
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext('2d');
     context.beginPath();
     context.arc(docx, docy, 5, 0, 2 * Math.PI);
     context.stroke();
-    context.fillStyle = '#89F401';
+    context.fillStyle = "#89F401";
     context.fill();
+
     var point = { x: docx, y: docy };
     pointsarray.push(point);
+
     if (pointsarray.length > 1) {
         let a = pointsarray.length;
         context.beginPath();
@@ -62,4 +70,5 @@ function addpoint(event) {
         context.stroke();
     }
 }
-myCanvas.addEventListener("click", addpoint);
+myCanvas.addEventListener('click', addpoint);
+
